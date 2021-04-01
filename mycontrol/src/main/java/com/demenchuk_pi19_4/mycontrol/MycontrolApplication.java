eaae63cd158cce93e41d48d8545775992d4599d5
Georgiy Demenchuk <demenchuk.george@protonmail.com>
2021-04-01 10:14:47 +0300

    Шаблон развернул

---
+++ mycontrol/src/main/java/com/demenchuk_pi19_4/mycontrol/MycontrolApplication.java
@@ -0,0 +1,13 @@
+package com.demenchuk_pi19_4.mycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycontrolApplication.class, args);
+	}
+
+}
---
+++ mycontrol/src/test/java/com/demenchuk_pi19_4/mycontrol/MycontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demenchuk_pi19_4.mycontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
